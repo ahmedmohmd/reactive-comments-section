ad740039043678779cabc58fed2853ab7af4b64c
ahmedmohmd <er.ahmed.3301@gmail.com>
2023-02-18 17:46:17 +0200

    almost finish (comment) function

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
-    colors: {
-      back: "#f5f6fa",
+    extend: {
+      colors: {
+        back: "#f5f6fa",
+        "violet-light": "#c8c7dd",
+        violet: "#5d6097",
+      },
     },
   },
   plugins: [],
